596fc75d38ac936d3aa55cd52a6e3fb656c40f3a
Cyril Couffe <cyril.couffe@yahoo.fr>
2024-02-10 15:15:02 +0100

    testing yaml file on i18next further

--- js/script.dev.js
+++ js/script.dev.js
@@ -9,15 +9,16 @@ function initI18next() {
         case 0:
           return _context.abrupt("return", new Promise(function (resolve, reject) {
             // Si i18next est déjà initialisé, renvoyer directement l'instance
-            console.log(i18nextInstance);
-
             if (i18nextInstance) {
               console.log("I18n déjà instanciée");
               resolve(i18nextInstance.t);
               return;
-            }
+            } // Créer une nouvelle instance i18next
 
-            i18next.use(i18nextBrowserLanguageDetector).use(i18nextHttpBackend).init({
+
+            var newInstance = i18next.createInstance(); // Configurer la nouvelle instance
+
+            newInstance.use(i18nextBrowserLanguageDetector).use(i18nextHttpBackend).init({
               debug: true,
               fallbackLng: "fr-FR",
               backend: {
@@ -28,9 +29,7 @@ function initI18next() {
                 reject(err);
               } else {
                 // Conserver l'instance i18next pour les appels ultérieurs
-                i18nextInstance = {
-                  t: t
-                };
+                i18nextInstance = newInstance;
                 resolve(t);
               }
             });
@@ -71,9 +70,11 @@ function changeContent(onHover, item) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function _callee() {
-  var t, buttons, img, h3, paragraph;
-  return regeneratorRuntime.async(function _callee$(_context3) {
+var buttons = document.querySelectorAll(".buttons li");
+
+function changeSelectPanelContent(button) {
+  var t, img, h3, paragraph;
+  return regeneratorRuntime.async(function changeSelectPanelContent$(_context3) {
     while (1) {
       switch (_context3.prev = _context3.next) {
         case 0:
@@ -82,70 +83,86 @@ document.addEventListener("DOMContentLoaded", function _callee() {
 
         case 2:
           t = _context3.sent;
-          buttons = document.querySelectorAll(".buttons li");
           img = document.querySelector(".container .main .content img");
           h3 = document.querySelector(".container .main .content h3");
           paragraph = document.querySelector(".container .main .content p");
-          buttons.forEach(function (button) {
-            button.addEventListener("click", function () {
-              buttons.forEach(function (btn) {
-                return btn.classList.remove("button-selected");
-              });
-              button.classList.add("button-selected"); // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
-              // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
-              // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
-              // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
-
-              switch (button.id) {
-                case "security":
-                  img.src = "img/security.avif";
-                  img.alt = "lock";
-                  h3.innerText = t("index.why_rust.select_panel.security.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.security.text");
-                  break;
-
-                case "performance":
-                  img.src = "img/speed.avif";
-                  img.alt = "a speed meter";
-                  h3.innerText = t("index.why_rust.select_panel.performance.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.performance.text");
-                  break;
-
-                case "concurrency":
-                  img.src = "img/race.avif";
-                  img.alt = "a race between athletes";
-                  h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.concurrency.text");
-                  break;
-
-                case "abstraction":
-                  img.src = "img/helpful.avif";
-                  img.alt = "a small robot";
-                  h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.abstraction.text");
-                  break;
-
-                case "ecosystem":
-                  img.src = "img/group.avif";
-                  img.alt = "a group of hands";
-                  h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
-                  break;
-
-                case "use_case":
-                  img.src = "img/mobile.avif";
-                  img.alt = "a mobile phone";
-                  h3.innerText = t("index.why_rust.select_panel.use_case.h3");
-                  paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
-                  break;
-              }
-            });
+          buttons.forEach(function (btn) {
+            return btn.classList.remove("button-selected");
           });
-          buttons[0].click();
+          button.classList.add("button-selected"); // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
+          // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
+          // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
+          // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
 
-        case 9:
+          console.log(button);
+          _context3.t0 = button.id;
+          _context3.next = _context3.t0 === "security" ? 12 : _context3.t0 === "performance" ? 17 : _context3.t0 === "concurrency" ? 22 : _context3.t0 === "abstraction" ? 27 : _context3.t0 === "ecosystem" ? 32 : _context3.t0 === "use_case" ? 37 : 42;
+          break;
+
+        case 12:
+          img.src = "img/security.avif";
+          img.alt = "lock";
+          h3.innerText = t("index.why_rust.select_panel.security.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.security.text");
+          return _context3.abrupt("break", 42);
+
+        case 17:
+          img.src = "img/speed.avif";
+          img.alt = "a speed meter";
+          h3.innerText = t("index.why_rust.select_panel.performance.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.performance.text");
+          return _context3.abrupt("break", 42);
+
+        case 22:
+          img.src = "img/race.avif";
+          img.alt = "a race between athletes";
+          h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.concurrency.text");
+          return _context3.abrupt("break", 42);
+
+        case 27:
+          img.src = "img/helpful.avif";
+          img.alt = "a small robot";
+          h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.abstraction.text");
+          return _context3.abrupt("break", 42);
+
+        case 32:
+          img.src = "img/group.avif";
+          img.alt = "a group of hands";
+          h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
+          return _context3.abrupt("break", 42);
+
+        case 37:
+          img.src = "img/mobile.avif";
+          img.alt = "a mobile phone";
+          h3.innerText = t("index.why_rust.select_panel.use_case.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
+          return _context3.abrupt("break", 42);
+
+        case 42:
         case "end":
           return _context3.stop();
+      }
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function _callee() {
+  return regeneratorRuntime.async(function _callee$(_context4) {
+    while (1) {
+      switch (_context4.prev = _context4.next) {
+        case 0:
+          buttons.forEach(function (button) {
+            button.addEventListener("click", function () {
+              changeSelectPanelContent(button);
+            });
+          });
+
+        case 1:
+        case "end":
+          return _context4.stop();
       }
     }
   });
--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ let i18nextInstance = null;
 async function initI18next() {
   return new Promise((resolve, reject) => {
     // Si i18next est déjà initialisé, renvoyer directement l'instance
-    console.log(i18nextInstance);
     if (i18nextInstance) {
       console.log("I18n déjà instanciée");
       resolve(i18nextInstance.t);
       return;
     }
 
-    i18next
+    // Créer une nouvelle instance i18next
+    const newInstance = i18next.createInstance();
+
+    // Configurer la nouvelle instance
+    newInstance
       .use(i18nextBrowserLanguageDetector)
       .use(i18nextHttpBackend)
       .init(
@@ -26,7 +29,7 @@ async function initI18next() {
             reject(err);
           } else {
             // Conserver l'instance i18next pour les appels ultérieurs
-            i18nextInstance = { t };
+            i18nextInstance = newInstance;
             resolve(t);
           }
         }
@@ -44,68 +47,65 @@ async function changeContent(onHover, item) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
+const buttons = document.querySelectorAll(".buttons li");
+async function changeSelectPanelContent(button) {
   const t = await initI18next();
-  const buttons = document.querySelectorAll(".buttons li");
   const img = document.querySelector(".container .main .content img");
   const h3 = document.querySelector(".container .main .content h3");
   const paragraph = document.querySelector(".container .main .content p");
+  buttons.forEach((btn) => btn.classList.remove("button-selected"));
+  button.classList.add("button-selected");
 
+  // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
+  // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
+  // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
+  // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
+  console.log(button);
+
+  switch (button.id) {
+    case "security":
+      img.src = "img/security.avif";
+      img.alt = "lock";
+      h3.innerText = t("index.why_rust.select_panel.security.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.security.text");
+      break;
+    case "performance":
+      img.src = "img/speed.avif";
+      img.alt = "a speed meter";
+      h3.innerText = t("index.why_rust.select_panel.performance.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.performance.text");
+      break;
+    case "concurrency":
+      img.src = "img/race.avif";
+      img.alt = "a race between athletes";
+      h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.concurrency.text");
+      break;
+    case "abstraction":
+      img.src = "img/helpful.avif";
+      img.alt = "a small robot";
+      h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.abstraction.text");
+      break;
+    case "ecosystem":
+      img.src = "img/group.avif";
+      img.alt = "a group of hands";
+      h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
+      break;
+    case "use_case":
+      img.src = "img/mobile.avif";
+      img.alt = "a mobile phone";
+      h3.innerText = t("index.why_rust.select_panel.use_case.h3");
+      paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
+      break;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
   buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      buttons.forEach((btn) => btn.classList.remove("button-selected"));
-      button.classList.add("button-selected");
-
-      // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
-      // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
-      // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
-      // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
-
-      switch (button.id) {
-        case "security":
-          img.src = "img/security.avif";
-          img.alt = "lock";
-          h3.innerText = t("index.why_rust.select_panel.security.h3");
-          paragraph.innerText = t("index.why_rust.select_panel.security.text");
-          break;
-        case "performance":
-          img.src = "img/speed.avif";
-          img.alt = "a speed meter";
-          h3.innerText = t("index.why_rust.select_panel.performance.h3");
-          paragraph.innerText = t(
-            "index.why_rust.select_panel.performance.text"
-          );
-          break;
-        case "concurrency":
-          img.src = "img/race.avif";
-          img.alt = "a race between athletes";
-          h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
-          paragraph.innerText = t(
-            "index.why_rust.select_panel.concurrency.text"
-          );
-          break;
-        case "abstraction":
-          img.src = "img/helpful.avif";
-          img.alt = "a small robot";
-          h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
-          paragraph.innerText = t(
-            "index.why_rust.select_panel.abstraction.text"
-          );
-          break;
-        case "ecosystem":
-          img.src = "img/group.avif";
-          img.alt = "a group of hands";
-          h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
-          paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
-          break;
-        case "use_case":
-          img.src = "img/mobile.avif";
-          img.alt = "a mobile phone";
-          h3.innerText = t("index.why_rust.select_panel.use_case.h3");
-          paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
-          break;
-      }
+    button.addEventListener("click", function () {
+      changeSelectPanelContent(button);
     });
   });
-  buttons[0].click();
 });
