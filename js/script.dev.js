2abd02227aee7cb789d4e4ef7407a01f7c6115ae
Cyril Couffe <cyril.couffe@yahoo.fr>
2024-02-10 14:24:46 +0100

    testing yaml file on i18next further

--- js/script.dev.js
+++ js/script.dev.js
@@ -7,7 +7,7 @@ function initI18next() {
         case 0:
           return _context.abrupt("return", new Promise(function (resolve, reject) {
             i18next.use(i18nextBrowserLanguageDetector).use(i18nextHttpBackend).init({
-              fallbackLng: "fr",
+              fallbackLng: "fr-FR",
               backend: {
                 loadPath: "locales/{{lng}}/{{ns}}.json"
               }
@@ -39,58 +39,15 @@ function changeContent(onHover, item) {
 
         case 2:
           t = _context2.sent;
-          console.log(t("head.final"));
           listItem = document.querySelector(".".concat(item));
-          _context2.t0 = item;
-          _context2.next = _context2.t0 === "web" ? 8 : _context2.t0 === "ai" ? 10 : _context2.t0 === "video-games" ? 12 : _context2.t0 === "embedded" ? 14 : _context2.t0 === "system" ? 16 : 18;
-          break;
 
-        case 8:
-          if (onHover) {
-            listItem.innerHTML = "\n              <div>\n                  <p>\n                  Rust est utilis\xE9 pour d\xE9velopper des applications web robustes,\n                  gr\xE2ce \xE0 son syst\xE8me de s\xE9curit\xE9 et ses performances\n                  exceptionnelles.\n                  </p>\n                  <a href=\"#\">En savoir plus</a>\n              </div>\n            ";
+          if (!onHover) {
+            listItem.innerHTML = t("index.use_case.".concat(item, ".h3"));
           } else {
-            listItem.innerHTML = "\n              <h3>D\xE9veloppement Web</h3>\n            ";
+            listItem.innerHTML = t("index.use_case.".concat(item, ".text"));
           }
 
-          return _context2.abrupt("break", 18);
-
-        case 10:
-          if (onHover) {
-            listItem.innerHTML = "\n                <div>\n                    <p>\n                      Les performances de Rust en font un choix prometteur pour le\n                      d\xE9veloppement d'applications d'intelligence artificielle.\n                    </p>\n                    <a href=\"#\">En savoir plus</a>\n                </div>\n              ";
-          } else {
-            listItem.innerHTML = "\n                <h3>Intelligence Artificielle</h3>\n              ";
-          }
-
-          return _context2.abrupt("break", 18);
-
-        case 12:
-          if (onHover) {
-            listItem.innerHTML = "\n                    <div>\n                        <p>\n                        Rust offre un contr\xF4le pr\xE9cis sur le mat\xE9riel, ce qui en fait un\n                        langage adapt\xE9 au d\xE9veloppement de jeux vid\xE9o haute performance.\n                        </p>\n                        <a href=\"#\">En savoir plus</a>\n                    </div>\n                  ";
-          } else {
-            listItem.innerHTML = "\n                    <h3>D\xE9veloppement de Jeux Vid\xE9o</h3>\n                  ";
-          }
-
-          return _context2.abrupt("break", 18);
-
-        case 14:
-          if (onHover) {
-            listItem.innerHTML = "\n                        <div>\n                            <p>\n                            Rust est utilis\xE9 pour le d\xE9veloppement de syst\xE8mes embarqu\xE9s,\n                offrant \xE0 la fois la s\xE9curit\xE9 et la performance n\xE9cessaire.\n                            </p>\n                            <a href=\"#\">En savoir plus</a>\n                        </div>\n                      ";
-          } else {
-            listItem.innerHTML = "\n                        <h3>Syst\xE8mes Embarqu\xE9s</h3>\n                      ";
-          }
-
-          return _context2.abrupt("break", 18);
-
-        case 16:
-          if (onHover) {
-            listItem.innerHTML = "\n                            <div>\n                                <p>\n                                Rust excelle dans la programmation syst\xE8me, offrant une\n                alternative s\xE9curis\xE9e aux langages traditionnels. Il a fait son\n                arriv\xE9e dans le Kernel Linux.\n                                </p>\n                                <a href=\"#\">En savoir plus</a>\n                            </div>\n                          ";
-          } else {
-            listItem.innerHTML = "\n                            <h3>Programmation Syst\xE8me</h3>\n                          ";
-          }
-
-          return _context2.abrupt("break", 18);
-
-        case 18:
+        case 5:
         case "end":
           return _context2.stop();
       }
@@ -98,62 +55,82 @@ function changeContent(onHover, item) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var buttons = document.querySelectorAll(".buttons li");
-  var img = document.querySelector(".container .main .content img");
-  var h3 = document.querySelector(".container .main .content h3");
-  var paragraph = document.querySelector(".container .main .content p");
-  buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      buttons.forEach(function (btn) {
-        return btn.classList.remove("button-selected");
-      });
-      button.classList.add("button-selected");
+document.addEventListener("DOMContentLoaded", function _callee() {
+  var t, buttons, img, h3, paragraph;
+  return regeneratorRuntime.async(function _callee$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          _context3.next = 2;
+          return regeneratorRuntime.awrap(initI18next());
 
-      switch (button.id) {
-        case "security":
-          img.src = "img/security.avif";
-          img.alt = "lock";
-          h3.innerText = "Sécurité de la Mémoire";
-          paragraph.innerText = "\n            Rust \xE9limine \xE9l\xE9gamment les erreurs de gestion de la m\xE9moire courantes, telles que les pointeurs nuls et les d\xE9bordements de tampon. Son syst\xE8me de possession et de gestion de la m\xE9moire garantit une s\xE9curit\xE9 sans sacrifier les performances, offrant ainsi une protection contre les failles de s\xE9curit\xE9 potentielles.\n            ";
-          break;
+        case 2:
+          t = _context3.sent;
+          buttons = document.querySelectorAll(".buttons li");
+          img = document.querySelector(".container .main .content img");
+          h3 = document.querySelector(".container .main .content h3");
+          paragraph = document.querySelector(".container .main .content p");
+          buttons.forEach(function (button) {
+            button.addEventListener("click", function () {
+              buttons.forEach(function (btn) {
+                return btn.classList.remove("button-selected");
+              });
+              button.classList.add("button-selected"); // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
+              // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
+              // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
+              // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
 
-        case "performance":
-          img.src = "img/speed.avif";
-          img.alt = "a speed meter";
-          h3.innerText = "Performances Exceptionnelles";
-          paragraph.innerText = "\n            Rust offre des performances exceptionnelles, rivalisant avec des langages bas niveau comme le C et le C++. Son mod\xE8le de propri\xE9t\xE9 unique et son syst\xE8me de gestion de la m\xE9moire permettent d'atteindre une efficacit\xE9 optimale sans compromis sur la s\xE9curit\xE9, en faisant un choix id\xE9al pour les applications n\xE9cessitant rapidit\xE9 et fiabilit\xE9.\n            ";
-          break;
+              switch (button.id) {
+                case "security":
+                  img.src = "img/security.avif";
+                  img.alt = "lock";
+                  h3.innerText = t("index.why_rust.select_panel.security.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.security.text");
+                  break;
 
-        case "concurrency":
-          img.src = "img/race.avif";
-          img.alt = "a race between athletes";
-          h3.innerText = "Concurrence Sans Douleur";
-          paragraph.innerText = "\n            La concurrence est native dans Rust gr\xE2ce \xE0 son mod\xE8le d'ex\xE9cution sans heurts. Rust int\xE8gre des fonctionnalit\xE9s de gestion de la concurrence facilitant le d\xE9veloppement d'applications parall\xE8les sans les tracas traditionnels. Cela rend Rust particuli\xE8rement adapt\xE9 aux syst\xE8mes distribu\xE9s et aux applications gourmandes en ressources.\n            ";
-          break;
+                case "performance":
+                  img.src = "img/speed.avif";
+                  img.alt = "a speed meter";
+                  h3.innerText = t("index.why_rust.select_panel.performance.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.performance.text");
+                  break;
 
-        case "abstraction":
-          img.src = "img/helpful.avif";
-          img.alt = "a small robot";
-          h3.innerText = "Abstraction Sans Perte de Performance";
-          paragraph.innerText = "\n            Contrairement \xE0 certains langages, Rust offre des abstractions puissantes tout en maintenant un contr\xF4le pr\xE9cis sur le mat\xE9riel. Les traits et les g\xE9n\xE9riques permettent de cr\xE9er du code g\xE9n\xE9rique sans sacrifier l'efficacit\xE9, offrant ainsi une flexibilit\xE9 sans compromettre la vitesse d'ex\xE9cution.\n            ";
-          break;
+                case "concurrency":
+                  img.src = "img/race.avif";
+                  img.alt = "a race between athletes";
+                  h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.concurrency.text");
+                  break;
 
-        case "ecosystem":
-          img.src = "img/group.avif";
-          img.alt = "a group of hands";
-          h3.innerText = "Écosystème Actif";
-          paragraph.innerText = "\n            Rust b\xE9n\xE9ficie d'une communaut\xE9 dynamique et d'un \xE9cosyst\xE8me en pleine expansion. Les crate (biblioth\xE8ques) disponibles offrent une grande vari\xE9t\xE9 de fonctionnalit\xE9s, facilitant le d\xE9veloppement de solutions rapides et robustes. La documentation et la communaut\xE9 garantissent une exp\xE9rience de d\xE9veloppement fluide.\n            ";
-          break;
+                case "abstraction":
+                  img.src = "img/helpful.avif";
+                  img.alt = "a small robot";
+                  h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.abstraction.text");
+                  break;
 
-        case "use-case":
-          img.src = "img/mobile.avif";
-          img.alt = "a mobile phone";
-          h3.innerText = "De multiples cas d'usage";
-          paragraph.innerText = "\n            Rust se distingue comme un langage de programmation moderne, offrant s\xE9curit\xE9, performances et facilit\xE9 de d\xE9veloppement. Que vous travailliez sur des projets syst\xE8me, des applications embarqu\xE9es ou des solutions cloud, Rust est un choix solide pour atteindre vos objectifs avec succ\xE8s.\n            ";
-          break;
+                case "ecosystem":
+                  img.src = "img/group.avif";
+                  img.alt = "a group of hands";
+                  h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
+                  break;
+
+                case "use_case":
+                  img.src = "img/mobile.avif";
+                  img.alt = "a mobile phone";
+                  h3.innerText = t("index.why_rust.select_panel.use_case.h3");
+                  paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
+                  break;
+              }
+            });
+          });
+          buttons[0].click();
+
+        case 9:
+        case "end":
+          return _context3.stop();
       }
-    });
+    }
   });
-  buttons[0].click();
 });
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ async function initI18next() {
       .use(i18nextHttpBackend)
       .init(
         {
-          fallbackLng: "fr",
+          fallbackLng: "fr-FR",
           backend: {
             loadPath: "locales/{{lng}}/{{ns}}.json",
           },
@@ -23,100 +23,16 @@ async function initI18next() {
 
 async function changeContent(onHover, item) {
   const t = await initI18next();
-  console.log(t("head.final"));
   var listItem = document.querySelector(`.${item}`);
-  switch (item) {
-    case "web":
-      if (onHover) {
-        listItem.innerHTML = `
-              <div>
-                  <p>
-                  Rust est utilisé pour développer des applications web robustes,
-                  grâce à son système de sécurité et ses performances
-                  exceptionnelles.
-                  </p>
-                  <a href="#">En savoir plus</a>
-              </div>
-            `;
-      } else {
-        listItem.innerHTML = `
-              <h3>Développement Web</h3>
-            `;
-      }
-      break;
-    case "ai":
-      if (onHover) {
-        listItem.innerHTML = `
-                <div>
-                    <p>
-                      Les performances de Rust en font un choix prometteur pour le
-                      développement d'applications d'intelligence artificielle.
-                    </p>
-                    <a href="#">En savoir plus</a>
-                </div>
-              `;
-      } else {
-        listItem.innerHTML = `
-                <h3>Intelligence Artificielle</h3>
-              `;
-      }
-      break;
-    case "video-games":
-      if (onHover) {
-        listItem.innerHTML = `
-                    <div>
-                        <p>
-                        Rust offre un contrôle précis sur le matériel, ce qui en fait un
-                        langage adapté au développement de jeux vidéo haute performance.
-                        </p>
-                        <a href="#">En savoir plus</a>
-                    </div>
-                  `;
-      } else {
-        listItem.innerHTML = `
-                    <h3>Développement de Jeux Vidéo</h3>
-                  `;
-      }
-      break;
-    case "embedded":
-      if (onHover) {
-        listItem.innerHTML = `
-                        <div>
-                            <p>
-                            Rust est utilisé pour le développement de systèmes embarqués,
-                offrant à la fois la sécurité et la performance nécessaire.
-                            </p>
-                            <a href="#">En savoir plus</a>
-                        </div>
-                      `;
-      } else {
-        listItem.innerHTML = `
-                        <h3>Systèmes Embarqués</h3>
-                      `;
-      }
-      break;
-    case "system":
-      if (onHover) {
-        listItem.innerHTML = `
-                            <div>
-                                <p>
-                                Rust excelle dans la programmation système, offrant une
-                alternative sécurisée aux langages traditionnels. Il a fait son
-                arrivée dans le Kernel Linux.
-                                </p>
-                                <a href="#">En savoir plus</a>
-                            </div>
-                          `;
-      } else {
-        listItem.innerHTML = `
-                            <h3>Programmation Système</h3>
-                          `;
-      }
-      break;
+  if (!onHover) {
+    listItem.innerHTML = t(`index.use_case.${item}.h3`);
+  } else {
+    listItem.innerHTML = t(`index.use_case.${item}.text`);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
+  const t = await initI18next();
   const buttons = document.querySelectorAll(".buttons li");
   const img = document.querySelector(".container .main .content img");
   const h3 = document.querySelector(".container .main .content h3");
@@ -127,54 +43,53 @@ document.addEventListener("DOMContentLoaded", function () {
       buttons.forEach((btn) => btn.classList.remove("button-selected"));
       button.classList.add("button-selected");
 
+      // img.src = t(`index.why_rust.select_panel.${button.id}.h3`);
+      // img.alt = t(`index.why_rust.select_panel.${button.id}.h3`);
+      // h3.innerText = t(`index.why_rust.select_panel.${button.id}.h3`);
+      // paragraph.innerText = t(`index.why_rust.select_panel.${button.id}.text`);
+
       switch (button.id) {
         case "security":
           img.src = "img/security.avif";
           img.alt = "lock";
-          h3.innerText = "Sécurité de la Mémoire";
-          paragraph.innerText = `
-            Rust élimine élégamment les erreurs de gestion de la mémoire courantes, telles que les pointeurs nuls et les débordements de tampon. Son système de possession et de gestion de la mémoire garantit une sécurité sans sacrifier les performances, offrant ainsi une protection contre les failles de sécurité potentielles.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.security.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.security.text");
           break;
         case "performance":
           img.src = "img/speed.avif";
           img.alt = "a speed meter";
-          h3.innerText = "Performances Exceptionnelles";
-          paragraph.innerText = `
-            Rust offre des performances exceptionnelles, rivalisant avec des langages bas niveau comme le C et le C++. Son modèle de propriété unique et son système de gestion de la mémoire permettent d'atteindre une efficacité optimale sans compromis sur la sécurité, en faisant un choix idéal pour les applications nécessitant rapidité et fiabilité.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.performance.h3");
+          paragraph.innerText = t(
+            "index.why_rust.select_panel.performance.text"
+          );
           break;
         case "concurrency":
           img.src = "img/race.avif";
           img.alt = "a race between athletes";
-          h3.innerText = "Concurrence Sans Douleur";
-          paragraph.innerText = `
-            La concurrence est native dans Rust grâce à son modèle d'exécution sans heurts. Rust intègre des fonctionnalités de gestion de la concurrence facilitant le développement d'applications parallèles sans les tracas traditionnels. Cela rend Rust particulièrement adapté aux systèmes distribués et aux applications gourmandes en ressources.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.concurrency.h3");
+          paragraph.innerText = t(
+            "index.why_rust.select_panel.concurrency.text"
+          );
           break;
         case "abstraction":
           img.src = "img/helpful.avif";
           img.alt = "a small robot";
-          h3.innerText = "Abstraction Sans Perte de Performance";
-          paragraph.innerText = `
-            Contrairement à certains langages, Rust offre des abstractions puissantes tout en maintenant un contrôle précis sur le matériel. Les traits et les génériques permettent de créer du code générique sans sacrifier l'efficacité, offrant ainsi une flexibilité sans compromettre la vitesse d'exécution.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.abstraction.h3");
+          paragraph.innerText = t(
+            "index.why_rust.select_panel.abstraction.text"
+          );
           break;
         case "ecosystem":
           img.src = "img/group.avif";
           img.alt = "a group of hands";
-          h3.innerText = "Écosystème Actif";
-          paragraph.innerText = `
-            Rust bénéficie d'une communauté dynamique et d'un écosystème en pleine expansion. Les crate (bibliothèques) disponibles offrent une grande variété de fonctionnalités, facilitant le développement de solutions rapides et robustes. La documentation et la communauté garantissent une expérience de développement fluide.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.ecosystem.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.ecosystem.text");
           break;
-        case "use-case":
+        case "use_case":
           img.src = "img/mobile.avif";
           img.alt = "a mobile phone";
-          h3.innerText = "De multiples cas d'usage";
-          paragraph.innerText = `
-            Rust se distingue comme un langage de programmation moderne, offrant sécurité, performances et facilité de développement. Que vous travailliez sur des projets système, des applications embarquées ou des solutions cloud, Rust est un choix solide pour atteindre vos objectifs avec succès.
-            `;
+          h3.innerText = t("index.why_rust.select_panel.use_case.h3");
+          paragraph.innerText = t("index.why_rust.select_panel.use_case.text");
           break;
       }
     });
