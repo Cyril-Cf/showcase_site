4a42a03e6e871f161b5d2083619ca5f52355bd06
Cyril Couffe <cyril.couffe@yahoo.fr>
2024-02-12 21:10:10 +0100

    fix CRLF / LF issues on sh file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,21 +3,21 @@ static-i18n -o . --file-format yaml --allow-html -l fr -i en -i fr html_template
 html-minifier --input-dir . --output-dir . --file-ext html --collapse-whitespace --minify-css true --minify-js true
 for f in js/*.js; do
     if [[ "$f" != *".min.js" ]]; then
-        filename=$(basename "$f" .js);
-        echo "Processing file: $f";
+        filename=$(basename "$f" .js)
+        echo "Processing file: $f"
         if [ ! -f "dist/js/$filename.min.js" ]; then
             if [ ! -d "dist/js" ]; then
                 mkdir -p dist/js
             fi
             touch dist/js/$filename.min.js
         fi
-        uglifyjs "$f" -o "dist/js/$filename.min.js";
+        uglifyjs "$f" -o "dist/js/$filename.min.js"
     fi
 done
 for f in css/*.sass; do
-    filename=$(basename "$f" .sass);
+    filename=$(basename "$f" .sass)
     if [[ ! "$filename" =~ ^_ ]]; then
-        echo "Processing file: $f";
+        echo "Processing file: $f"
         node-sass "$f" "dist/css/$filename.min.css" --output-style compressed
     fi
 done
